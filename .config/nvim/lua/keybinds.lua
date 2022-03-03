47e8646f9740d37c8ab6956179bb1f7f71fdf0c6
Noah Citron <noah@jeff.org>
2022-03-03 16:00:29 -0500

    buffers

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = ","
+
+-- NerdTree
 vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>N', ':NERDTreeFind<CR>', {})
+
+-- Buffers
+vim.api.nvim_set_keymap('n', ';', ':BufferLineCyclePrev<CR>', {})
+vim.api.nvim_set_keymap('n', '\'', ':BufferLineCycleNext<CR>', {})
+vim.api.nvim_set_keymap('n', '<C-w>', ':Bdelete<CR>', {})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,8 +8,20 @@ Plug('gko/vim-coloresque')
 Plug('arcticicestudio/nord-vim')
 Plug('fatih/vim-go', {['do'] = ':GoUpdateBinaries'})
 Plug('preservim/nerdtree')
+Plug('christoomey/vim-tmux-navigator')
+Plug('kyazdani42/nvim-web-devicons')
+Plug('akinsho/bufferline.nvim')
+Plug('moll/vim-bbye')
 
 vim.call('plug#end')
 
 -- NerdTree
 vim.g.NERDTreeMinimalUI = 1
+
+-- Buffline
+vim.opt.termguicolors = true
+require("bufferline").setup{
+    options = {
+        offsets = {{ filetype = "nerdtree", text = "File Explorer", highlight = "Directory", text_align = "left"}}
+    }
+}

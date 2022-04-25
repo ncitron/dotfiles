f594e10a92c33c628ae0702acbb9068118da3aa6
Noah Citron <noah@jeff.org>
2022-04-25 17:31:26 -0400

    fix clipboard override

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -8,3 +8,10 @@ vim.api.nvim_set_keymap('n', '<leader>N', ':NERDTreeFind<CR>', {})
 vim.api.nvim_set_keymap('n', ';', ':BufferLineCyclePrev<CR>', {})
 vim.api.nvim_set_keymap('n', '\'', ':BufferLineCycleNext<CR>', {})
 vim.api.nvim_set_keymap('n', '<C-w>', ':Bdelete<CR>', {})
+
+-- Fix clipboard override
+vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true })
+vim.api.nvim_set_keymap('n', 'D', '"_D', { noremap = true })
+vim.api.nvim_set_keymap('v', 'd', '"_d', { noremap = true })
+vim.api.nvim_set_keymap('n', 'c', '"_c', { noremap = true })
+

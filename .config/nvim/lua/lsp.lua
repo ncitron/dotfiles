fb1d488bbabb32a252811dc35ceeccb5535e8d49
Noah Citron <noah@jeff.org>
2022-04-26 17:09:13 -0400

    fix go lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -38,7 +38,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'solidity_ls' }
+local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'solidity_ls', 'gopls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
@@ -48,3 +48,4 @@ for _, lsp in pairs(servers) do
     }
   }
 end
+

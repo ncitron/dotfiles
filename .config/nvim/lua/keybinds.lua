d575b78f41a468c24d9fdf4f11201e6f6a1f7f83
Noah Citron <noah@jeff.org>
2022-05-10 20:27:18 -0400

    add :noh binding and silence others

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,13 +1,16 @@
 vim.g.mapleader = " "
 
+-- General
+vim.api.nvim_set_keymap('n', '<esc>', ':noh<CR>', { silent = true })
+
 -- NerdTree
 vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>N', ':NERDTreeFind<CR>', {})
 
 -- Buffers
-vim.api.nvim_set_keymap('n', '<leader>;', ':BufferLineCyclePrev<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>\'', ':BufferLineCycleNext<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>w', ':Bdelete<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>;', ':BufferLineCyclePrev<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>\'', ':BufferLineCycleNext<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>w', ':Bdelete<CR>', { silent = true })
 
 -- Fix clipboard override
 vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true })
